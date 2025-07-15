7e662bd1463ec64483e4b02b6a62e0597c68d3c5
Andrii <semtrololo@gmail.com>
2025-07-15 17:25:50 +0200

    add metadata in /notes/action/create

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,5 +1,17 @@
 import NoteForm from "@/components/NoteForm/NoteForm";
 import css from "./CreateNote.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NoteHub: Create Note",
+  description: "Create new Note",
+  openGraph: {
+    title: "NoteHub: Create Note",
+    description: "Create new Note",
+    url: "http://localhost:3000/notes/action/create",
+    images: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+  },
+};
 
 export default function CreateNote() {
   return (
